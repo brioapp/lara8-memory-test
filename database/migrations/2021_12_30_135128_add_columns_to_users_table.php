beff77041ef369e3053fd65e99ce401f800baf6c
brioapp <cdrlee@gmail.com>
2021-12-30 21:57:13 +0800

    feat: make users table migration

---
+++ database/migrations/2021_12_30_135128_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->nullable()->after('name');
+            $table->string('phone')->unique()->nullable();
+            $table->string('avatar')->nullable()->default('users/default.png');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'username',
+                'phone',
+                'avatar',
+            ]);
+        });
+    }
+}
